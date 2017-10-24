7233a3c772b64df568a91675f6b69132b520583b
PrithMDP <prithvijeet.y@gmail.com>
2017-10-24 07:19:59 -0400

    Update ziptry2.hpp

--- ziptry2.hpp
+++ ziptry2.hpp
@@ -5,35 +5,38 @@
 #include <functional>
 #include <deque>
 
-using std::cout;
-using std::endl;
-using std::vector;
-using std::deque;
-using std::get;
-using std::tuple;
+using namespace std;
 
 
-// do i need to forward args.begin() to make
+
+//how to forward the vector/deque?
+
+/* i am trying 2 make 2 tuples one with .begin() for each of the containers
+   other with .end() for the same containers.
+   
+   the containers arent being passed by ref here so my output is messed up (or so i think)
+*/
+
 template <typename... Args>
 auto gen_tuple_start(Args&&... args) {
-    return std::make_tuple(std::begin(std::forward<Args>(args))...);
+    return std::make_tuple((std::forward<Args>(args).begin())...);
 }
 
 template <typename... Args>
 auto gen_tuple_end(Args&&... args) {
-    return std::make_tuple(std::end(std::forward<Args>(args))...);
+    return std::make_tuple((std::forward<Args>(args).end())...);
 }
 
 template <typename... Args>
 auto gen_tuple_start_wrapper(Args&&... args)
 {
-    return gen_tuple_start(std::forward<Args>(args)... );
+    return gen_tuple_start(forward<Args>(args)... );
 }
 
 template <typename... Args>
 auto gen_tuple_end_wrapper(Args&&... args)
 {
-    return gen_tuple_end(std::forward<Args>(args)... );
+    return gen_tuple_end(forward<Args>(args)... );
 }
 
 
@@ -43,7 +46,9 @@ int main()
     deque<int> vec1{4,5,6};
     auto s = gen_tuple_start_wrapper(vec,vec1);
     auto e = gen_tuple_end_wrapper(vec,vec1);
-
+    
+    // now iterating over
+    
     auto f = get<0>(s);
     
     auto g = get<0>(e);
